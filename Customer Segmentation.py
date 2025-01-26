57ea36c6dada92c4e7959d376fe3969870813bd6
Altamash Akber <89542145+Altamash38@users.noreply.github.com>
2025-01-26 19:50:37 +0500

    Update Customer Segmentation.py

--- Customer Segmentation.py
+++ Customer Segmentation.py
@@ -5,6 +5,7 @@
 
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
@@ -14,14 +15,14 @@ warnings.filterwarnings('ignore')
 # In[2]:
 
 
-import numpy as np
-
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 # In[3]:
 
 
-from sklearn.cluster import KMeans 
-from sklearn.metrics import silhouette_score
+from google.colab import drive
+drive.mount('/content/drive')
 
 
 # In[4]:
@@ -33,217 +34,119 @@ df= pd.read_csv("D:\Data sets\Mall_Customers.csv")
 # In[5]:
 
 
-df.head(20)
-
-
-# In[6]:
-
+df.head()
 
 df.rename(columns = {'Annual Income (k$)':'Annual_Income_inThousandUSD','Spending Score (1-100)':'Spending_Score' }, inplace = True)
 
-
-# In[7]:
-
-
 df.head()
 
-
-# In[8]:
-
+# Statistical Analysis
 
 df.describe()
 
-
-# In[9]:
-
-
 sns.distplot(df['Annual_Income_inThousandUSD'])
-
-
-# In[10]:
-
 
 sns.histplot(df['Annual_Income_inThousandUSD'])
 
-
-# In[11]:
-
-
-df.columns
-
-
-# In[12]:
-
-
 columns=['Age', 'Annual_Income_inThousandUSD','Spending_Score']
-
-
-# In[13]:
-
 
 for i in columns:
  plt.figure()
  sns.distplot(df[i])
 
-
-# In[14]:
-
-
-sns.kdeplot(df['Annual_Income_inThousandUSD'],shade=True,hue=df['Gender']);
-
-
-# In[15]:
+plt.figure(figsize=(8, 6))  # Increase the width and height of the plot
+df.boxplot(column=['Age', 'Annual_Income_inThousandUSD', 'Spending_Score'], grid=False)
+plt.title("Box Plot of Age, Annual Income and Spend Score")
+plt.ylabel("Value")
+# plt.xticks(rotation=45)  # If labels are overlapping
+plt.show()
 
 
-for i in columns:
- plt.figure()
- sns.kdeplot(df[i],shade=True,hue=df['Gender']);
+from scipy.stats import skew
+
+spend_skewness = skew(df['Spending_Score'])
+print("Skewness of Spend Score:", spend_skewness)
+
+income_skewness = skew(df['Annual_Income_inThousandUSD'])
+print("Skewness of Annual Income:", income_skewness)
+
+Age_skewness = skew(df['Age'])
+print("Skewness of Age:", Age_skewness)
+
+age_bins = [18, 25, 35, 45, 55, 65, 70]  # Define age ranges
+age_labels = ['18-24', '25-34', '35-44', '45-54', '55-64', '65-70']  # Labels for the groups
+
+df['Age_Group'] = pd.cut(df['Age'], bins=age_bins, labels=age_labels, right=False)
+
+age_group_counts = df.groupby('Age_Group')['CustomerID'].count()
+print(age_group_counts)
+
+income_bins = [15, 46 , 77 , 108 ,137]  # Define age ranges
+income_labels = ['15-45', '46-76', '77-107', '108-137']  # Labels for the groups
+
+df['Income_Group'] = pd.cut(df['Annual_Income_inThousandUSD'], bins=income_bins, labels=income_labels, right=False)
+
+income_group_counts = df.groupby('Income_Group')['CustomerID'].count()
+print(income_group_counts)
+
+# Exploratory Analysis of Age, Spending, and Income by Gender
 
 
-# In[16]:
-
+# sns.kdeplot(df['Annual_Income_inThousandUSD'],shade=True, hue=df['Gender']);
+sns.kdeplot(data=df, x='Annual_Income_inThousandUSD', hue='Gender', shade=True);
 
 df['Gender'].value_counts(normalize=True)
 
+# BI Variate Analysis
 
-# # Bivariate Analysis
-
-# In[17]:
-
+for i in columns:
+    plt.figure()
+    sns.kdeplot(data=df, x=i, hue='Gender', shade=True); # Specify data and x explicitly
 
 #sns.scatterplot(data=df,x='Annual Income (k$)',y='Spending Score (1-100)',hue='Age');
 sns.scatterplot(data=df,x='Annual_Income_inThousandUSD',y='Spending_Score');
-
-
-# In[18]:
-
 
 x=df.copy()
 y=x.drop('CustomerID',axis=1)
 sns.pairplot(y,hue='Gender');
 
+# df.groupby(['Gender'])['Age', 'Annual_Income_inThousandUSD','Spending_Score'].mean()
+df.groupby(['Gender'])[['Age', 'Annual_Income_inThousandUSD','Spending_Score']].mean() # Changed from tuple to list for column selection
 
-# In[19]:
+df.groupby(['Gender'])[['Age']].describe()
 
+df.groupby(['Gender'])[['Annual_Income_inThousandUSD']].describe()
 
-df.groupby(['Gender'])['Age', 'Annual_Income_inThousandUSD','Spending_Score'].mean()
+df.groupby(['Gender'])[['Spending_Score']].describe()
 
+z=y.copy()
+z = y.drop(['Gender','Age_Group', 'Income_Group'], axis=1)
+sns.heatmap(z.corr(),annot=True);
 
-# In[19]:
+#Univariate Clustering
 
-
-#df.corr()
-sns.heatmap(y.corr(),annot=True);
-
-
-# # Clusterin(Univariate)
-
-# In[20]:
-
-
-df.head()
-
-
-# In[21]:
-
+dff=df.copy().drop(['Age_Group','Income_Group'],axis=1)
+dff.head()
 
 clustering1= KMeans(5)
 
-
-# In[22]:
-
-
-#type(clustering1)
-type('Annual_Income_inThousandUSD')
-
-
-# In[23]:
-
-
-df["Annual_Income_inThousandUSD"] = pd.to_numeric(df["Annual_Income_inThousandUSD"])
-df["Spending_Score"] = pd.to_numeric(df["Spending_Score"])
-
-
-# In[26]:
-
-
-df.astype({'Annual_Income_inThousandUSD': 'float'}).dtypes
-print(df.dtypes)
-
-
-# In[27]:
-
-
-df["Annual_Income_inThousandUSD"].dtypes
-
-
-# In[22]:
-
-
-a=df.copy()
-#z=a.drop(df.columns[1], axis = 1, inplace=True)
-
-
-# # Drop Once
-
-# In[23]:
-
-
-#a=a.drop(df.columns[1], axis = 1)
+a=dff.copy().drop(dff.columns[1], axis = 1)
 a.head()
-
-
-# In[24]:
-
 
 clustering1.fit(a[['Annual_Income_inThousandUSD']])
 
-
-# In[25]:
-
-
 clustering1.labels_
-
-
-# In[26]:
-
-
-df["Income_cluster"]=clustering1.labels_
-
-
-# In[27]:
-
-
-df["Income_cluster"].value_counts()
-
-
-# In[28]:
-
 
 clusters = a.copy()
 clusters['cluster_pred']=clustering1.fit_predict(a)
 
-
-# In[29]:
-
-
 clusters['cluster_pred'].value_counts()
-
-
-# In[30]:
-
 
 clusters.head(5)
 
+dff["Income_cluster"]=clustering1.labels_
 
-# In[31]:
-
-
-df.head()
-
-
-# In[32]:
-
+dff.head()
 
 plt.figure(figsize=(5,5))
 plt.scatter(clusters['Annual_Income_inThousandUSD'],clusters['Spending_Score'],c=clusters['cluster_pred'],cmap='rainbow')
@@ -252,221 +155,109 @@ plt.xlabel("Annual Income")
 plt.ylabel("Spending Score")
 plt.show()
 
+Inertia is simply a measure that suggests how well a data set is clustered
 
-# In[94]:
+We will find the K numbers of clusters manually by the Elbow method.
 
+The elbow method illustrates that the optimal number of clusters is where the inertia value starts decreasing slowly.
 
 inertia_scores=[]
 for i in range(1,11):
-    kmeans=KMeans(n_clusters=i)
-    kmeans.fit(a)
-    inertia_scores.append(kmeans.inertia_)
-
-
-# In[95]:
-
+ kmeans=KMeans(n_clusters=i)
+ kmeans.fit(a)
+ inertia_scores.append(kmeans.inertia_)
 
 plt.plot(range(1,11),inertia_scores)
 
-
-# In[34]:
-
-
-df.groupby("Income_cluster")["Age","Annual_Income_inThousandUSD","Spending_Score"].mean()
-
-
-# In[35]:
-
+# dff.groupby("Income_cluster")["Age","Annual_Income_inThousandUSD","Spending_Score"].mean()
+dff.groupby("Income_cluster")[["Age","Annual_Income_inThousandUSD","Spending_Score"]].mean()
 
 score = silhouette_score(a, clustering1.labels_, metric='euclidean')
-
-
-# In[36]:
-
-
 print(score)
-
-
-# In[37]:
-
 
 score = silhouette_score(clusters, clustering1.labels_, metric='euclidean')
 print(score)
 
-
-# In[38]:
-
-
-avg_data = df.groupby(['Income_cluster'],
-as_index=False).mean()
+avg_data = dff.groupby(['Income_cluster'], as_index=False).agg({
+    'Age': 'mean',
+    'Annual_Income_inThousandUSD': 'mean',
+    'Spending_Score': 'mean'
+})
 print(avg_data)
 
-
-# # Bivariate Clustering
-
-# In[39]:
+#Bi Variate Clustering
 
 
 clustering2=KMeans(5)
 clustering2.fit(a[['Annual_Income_inThousandUSD','Spending_Score']])
-df["Income_nd_SpendingScore_cluster"]=clustering2.labels_
-df.head()
+clusters["cluster_pred2"]=clustering2.labels_
+clusters.head()
 
+dff["Income_nd_SpendScore_cluster"]=clustering2.labels_
 
-# In[45]:
-
+Finding Inertia Score
 
 inertia_score2=[]
 for i in range(1,11):
-    kmeans2=KMeans(n_clusters=i)
-    kmeans2.fit(a[['Annual_Income_inThousandUSD','Spending_Score']])
-    inertia_score2.append(kmeans2.inertia_)
-
-
-# In[46]:
+ kmeans2=KMeans(n_clusters=i)
+ kmeans2.fit(a[['Annual_Income_inThousandUSD','Spending_Score']])
+ inertia_score2.append(kmeans2.inertia_)
 
 
 plt.plot(range(1,11),inertia_score2)
 
-
-# In[40]:
-
-
-df["Income_nd_SpendingScore_cluster"].value_counts()
-
-
-# In[41]:
-
+clusters["cluster_pred2"].value_counts()
 
 centers=pd.DataFrame(clustering2.cluster_centers_)
 centers.columns=['x','y']
 
-
-# In[42]:
-
-
 plt.figure(figsize=(8,6))
 plt.scatter(x=centers['x'],y=centers['y'],s=50,c='black',marker='*')
-sns.scatterplot(data=df,x='Annual_Income_inThousandUSD', y='Spending_Score', hue='Income_nd_SpendingScore_cluster',palette='tab10')
+sns.scatterplot(data=clusters,x='Annual_Income_inThousandUSD', y='Spending_Score', hue='cluster_pred2',palette='tab10')
 
+pd.crosstab(clusters['cluster_pred2'],df['Gender'],normalize='index')
 
-# In[43]:
-
-
-pd.crosstab(df['Income_nd_SpendingScore_cluster'],df['Gender'],normalize='index')
-
-
-# In[44]:
-
-
-df.groupby("Income_nd_SpendingScore_cluster")["Age","Annual_Income_inThousandUSD","Spending_Score"].mean()
-
-
-# In[45]:
-
+clusters.groupby("cluster_pred2")[["Age","Annual_Income_inThousandUSD","Spending_Score"]].mean()
 
 score = silhouette_score(a, clustering2.labels_, metric='euclidean')
-
-
-# In[46]:
-
-
 print(score)
 
-
-# # Multivariate Clustering
-
-# In[47]:
-
+# Multivariate Clustering
 
 from sklearn.preprocessing import StandardScaler
 
+scale = StandardScaler()
 
-# In[48]:
+dff['Gender_Male'] = dff['Gender'].replace({'Male': 1, 'Female': 0})
 
-
-scale= StandardScaler()
-
-
-# In[49]:
-
-
-dff=pd.get_dummies(df,drop_first=True)
 dff.head()
 
+Dum_dff=pd.get_dummies(dff,drop_first=True)
+Dum_dff.head()
 
-# In[50]:
+Dum_dff.columns
 
+Dum_dff= Dum_dff[['Age', 'Annual_Income_inThousandUSD', 'Spending_Score', 'Gender_Male']]
+Dum_dff.head()
 
-dff.columns
+Dum_dff= scale.fit_transform(Dum_dff)
 
-
-# In[51]:
-
-
-dff= dff[['Age', 'Annual_Income_inThousandUSD', 'Spending_Score', 'Gender_Male']]
-dff.head()
-
-
-# In[52]:
-
-
-dff= scale.fit_transform(dff)
-
-
-# In[53]:
-
-
-dff=pd.DataFrame(scale.fit_transform(dff))
-dff.head()
-
-
-# In[143]:
-
+Dum_dff=pd.DataFrame(scale.fit_transform(Dum_dff))
+Dum_dff.head()
 
 inertia_score3=[]
 for i in range(1,11):
-    kmeans3=KMeans(n_clusters=i)
-    kmeans3.fit(dff)
-    inertia_score3.append(kmeans3.inertia_)
+ kmeans3=KMeans(n_clusters=i)
+ kmeans3.fit(Dum_dff)
+ inertia_score3.append(kmeans3.inertia_)
+
 plt.plot(range(1,11),inertia_score2)
 
-
-# In[54]:
-
-
-df.to_csv('Clusteringdata.csv')
-
-
-# In[55]:
-
-
 clustering3=KMeans(5)
-clustering3.fit(dff)
-dff["multi_cluster"]=clustering3.labels_
-dff.head()
+clustering3.fit(Dum_dff)
 
+Dum_dff = pd.DataFrame(Dum_dff)
+Dum_dff["multi_cluster"]=clustering3.labels_
+Dum_dff.head()
 
-# In[56]:
-
-
-dff["multi_cluster"].value_counts()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+Dum_dff["multi_cluster"].value_counts()
